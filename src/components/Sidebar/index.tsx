6c11650f4c8a0641400673c8843eec372d954c79
jmakhmudov <makhmudov@tuta.io>
2024-09-01 19:56:12 +0500

    rem username

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,25 +4,12 @@ import logo from '@/../public/images/snr-logo.svg';
 import Image from "next/image";
 import Link from "next/link";
 
-import { store } from "@/store";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 import { FiSettings } from "react-icons/fi";
-import { useSnapshot } from "valtio";
 import { navLink } from "./nav-links";
-import { syncUserData } from '@/utils/api/syncUserData';
 
 export default function Sidebar() {
-  const { user } = useSnapshot(store);
   const pathname = usePathname();
-
-  useEffect(() => {
-    const getUser = async () => {
-      await syncUserData();
-    }
-
-    getUser();
-  }, [])
 
   return (
     <div className=" fixed left-0 top-0 bottom-0 border-r border-gray-light-0 flex flex-col items-center justify-between bg-white py-8 w-[210px] z-[100] select-none">
@@ -56,7 +43,7 @@ export default function Sidebar() {
       <div className="w-full">
         <Link href={'/account'} className="flex items-center gap-3 cursor-pointer px-10 py-3">
           <div className="w-6 h-6 aspect-square bg-blue rounded-full"></div>
-          <div className="text-xs font-medium">{user.full_name || 'loading'}</div>
+          <div className="text-xs font-medium">Аккаунт</div>
         </Link>
 
         <div className="flex items-center gap-3 cursor-pointer group px-10 py-3">
